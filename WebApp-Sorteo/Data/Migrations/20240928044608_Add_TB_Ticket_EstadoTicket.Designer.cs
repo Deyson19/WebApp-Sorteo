9a04999e4c1b1f582f943c1801cdeb500b412bd5
Deyson Venté <deiandresvent@hotmail.com>
2024-09-28 00:07:30 -0500

    Se agrega el campo de estado del ticket a la base de datos.

--- WebApp-Sorteo/Data/Migrations/20240928044608_Add_TB_Ticket_EstadoTicket.Designer.cs
+++ WebApp-Sorteo/Data/Migrations/20240928044608_Add_TB_Ticket_EstadoTicket.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp_Sorteo.Data;
 
@@ -11,9 +12,11 @@ using WebApp_Sorteo.Data;
 namespace WebApp_Sorteo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240928044608_Add_TB_Ticket_EstadoTicket")]
+    partial class Add_TB_Ticket_EstadoTicket
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
