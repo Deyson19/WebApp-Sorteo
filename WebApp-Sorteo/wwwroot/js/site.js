a1f056d32a85547ce4b9fe35fcea2808b21bdf7d
Deyson Venté <deiandresvent@hotmail.com>
2024-10-03 09:57:27 -0500

    Scripts globales para aplicar estilos y data table a la respectiva tabla de datos por id

--- WebApp-Sorteo/wwwroot/js/site.js
+++ WebApp-Sorteo/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿//*Obtener la tabla con id=tblDatos sin importar que vista sea. Script global
+const table = document.querySelector("#tblDatos");
+const tableClass = [
+  "table-bordered",
+  "table-hover",
+  "table-inverse",
+  "table-dark",
+  "align-middle",
+  "table-responsive",
+  "shadow-lg",
+];
+if (table) {
+  tableClass.forEach((x) => {
+    table.classList.add(x);
+  });
+  $(document).ready(function () {
+    $("#tblDatos").DataTable({
+      language: {
+        url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json",
+      },
+      aLengthMenu: [
+        [15, 50, 100, 200, -1],
+        [15, 50, 100, 200, "Todos"],
+      ],
+    });
+  });
+}
