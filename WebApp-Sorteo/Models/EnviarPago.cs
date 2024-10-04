02e1792fc49793ee43a66ba858f8575e0d70fa3d
Deyson Venté <deiandresvent@hotmail.com>
2024-10-03 22:17:52 -0500

    Modelo dto con anotaciones de datos antes de ir a pagar el ticket.

--- WebApp-Sorteo/Models/EnviarPago.cs
+++ WebApp-Sorteo/Models/EnviarPago.cs
@@ -1,5 +1,7 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApp_Sorteo.Models
 {
     public class EnviarPago
@@ -18,5 +20,30 @@ namespace WebApp_Sorteo.Models
         // public required string City { get; set; }
         // public required string FederalUnit { get; set; }
         // public required string PhoneAreaCode { get; set; }
+    }
+    public class EnviarPagoDTO
+    {
+        //Datos para solicitar al usuario en el formulario para procesar el pago con mercado pago
+        [Display(Name = "Correo")]
+        public required string Email { get; set; }
+        [Display(Name = "Número Telefónico")]
+        public required string PhoneNumber { get; set; }
+        [Display(Name = "Tipo de Persona")]
+        public required string PersonType { get; set; }
+        [Display(Name = "Tipo de Identificación")]
+        public string IdentificationType { get; set; }
+        [Display(Name = "Número de Identificación")]
+        public required string IdentificationNumber { get; set; }
+        [Display(Name = "Código Postal")]
+        public required string ZipCode { get; set; }
+        [Display(Name = "Nombre de la calle")]
+        public required string StreetName { get; set; }
+        [Display(Name = "Número de la calle")]
+        public required string StreetNumber { get; set; }
+        [Display(Name = "Barrio")]
+        public required string Neighborhood { get; set; }
+        [Display(Name = "Ciudad")]
+        public required string City { get; set; }
+        public required string PhoneAreaCode { get; set; }
     }
 }
