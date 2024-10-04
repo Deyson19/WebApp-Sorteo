2a6a3dbbe86738cf13faed10430d27955ca7ff44
Deyson Venté <deiandresvent@hotmail.com>
2024-10-03 22:18:54 -0500

    Migraciones para el campo de PrefId para la tabla de pagos.

---
+++ WebApp-Sorteo/Data/Migrations/20241004024640_TB_Pagos_Campo_PrefId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp_Sorteo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TB_Pagos_Campo_PrefId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrefId",
+                table: "Pagos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrefId",
+                table: "Pagos");
+        }
+    }
+}
