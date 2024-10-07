02d8deede294f1286a29aa52e31a73bafe4af945
Deyson Venté <deiandresvent@hotmail.com>
2024-10-07 17:19:25 -0500

    Actualizar el estado del ticket y del pago una vez completada la compra.

--- WebApp-Sorteo/Controllers/EstadoPagoController.cs
+++ WebApp-Sorteo/Controllers/EstadoPagoController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Sorteo.Data;
+using WebApp_Sorteo.Helpers;
 using WebApp_Sorteo.Models;
 
 namespace WebApp_Sorteo.Controllers
@@ -8,27 +12,59 @@ namespace WebApp_Sorteo.Controllers
     public class EstadoPagoController : Controller
     {
         private readonly ILogger<EstadoPagoController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
 
-        public EstadoPagoController(ILogger<EstadoPagoController> logger)
+        public EstadoPagoController(ILogger<EstadoPagoController> logger, ApplicationDbContext context, IEmailSender emailSender)
         {
             _logger = logger;
+            _context = context;
+            _emailSender = emailSender;
         }
-        [HttpGet("Success")]
+        public IActionResult Index() => View();
+        
         public async Task<IActionResult> Success([FromQuery] PaymentResponse paymentResponse)
         {
+            if (ModelState.IsValid)
+            {
+                var pago = await _context.Pagos.FirstOrDefaultAsync(x=>x.PrefId == paymentResponse.payment_id);
+                if (pago == null)
+                {
+                    TempData[DS.Error] = "El pago no existe";
+                    return RedirectToAction("Index","Home");
+                }
+                pago.MetodoPago = paymentResponse.payment_type;
+                pago.EstadoPago = EstadoPago.Pagado;
+                pago.FechaPago = DateTime.Now;
+                pago.DetallesPago = "Pago realizado correctamente";
+
+                var ticket = await _context.Tickets.Include(c=>c.Usuario).Include(x=>x.Sorteo).FirstOrDefaultAsync(x => x.Id == pago.TicketId);
+                if (ticket == null)
+                {
+                    TempData[DS.Error] = "No existe un ticket para ese pago";
+                    return RedirectToAction("Index","Home");
+                }
+                ticket.EstadoPago = EstadoPago.Pagado;
+                ticket.FechaCompra = DateTime.Now;
+                ticket.EstadoTicket = EstadoTicket.Inactivo;
+                ticket.MontoPagado = pago.Monto;
+
+                await _context.SaveChangesAsync();
+                var cuerpoCorreo = $"Se ha pagado el ticket: {ticket.NumeroTicket} y " +
+                    $"ya estas participando en el sorteo de: {ticket.Sorteo!.Descripcion}, Mucha suerte";
+
+                await _emailSender.SendEmailAsync(ticket.Usuario!.Email!,"Haz realizado un pago.",cuerpoCorreo);
+            }
             return Json(paymentResponse);
         }
-        [HttpGet("Failure")]
         public async Task<IActionResult> Failure([FromQuery] PaymentResponse paymentResponse)
         {
             return Json(paymentResponse);
         }
-        [HttpGet("Pending")]
         public async Task<IActionResult> Pending([FromQuery] PaymentResponse paymentResponse)
         {
             return Json(paymentResponse);
         }
-        [HttpGet("Notification")]
         public async Task<IActionResult> Notification([FromQuery] PaymentResponse paymentResponse)
         {
             return Json(paymentResponse);
