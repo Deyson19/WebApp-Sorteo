fdcd280c46250dfc2908d357f45c2074bd6e42d0
Deyson Venté <deiandresvent@hotmail.com>
2024-11-25 17:03:37 -0500

    quitada la autorización y simplifica el retorno de vistas

--- WebApp-Sorteo/Controllers/HomeController.cs
+++ WebApp-Sorteo/Controllers/HomeController.cs
@@ -1,32 +1,21 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApp_Sorteo.Models;
-using WebApp_Sorteo.Models.Helpers;
 
 namespace WebApp_Sorteo.Controllers
 {
-    //[Authorize(Roles = Roles.Role_Admin)]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
-
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [Authorize(Roles = Roles.Role_Admin)]
-        public IActionResult Privacy()
-        {
-            _logger.LogError($"Acceso denegado para {User.Identity!.Name}, no es admin.");
-            return View();
-        }
+        [ResponseCache(Duration = 100, Location = ResponseCacheLocation.Client, NoStore = false)]
+        public IActionResult Index() => View();
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
