389e463a0a23faa75f4d7fb04bd66df3f4611c5e
Deyson Venté <deiandresvent@hotmail.com>
2024-11-26 12:03:13 -0500

    Campos del Db context requeridos para quitar incidencias

--- WebApp-Sorteo/Data/ApplicationDbContext.cs
+++ WebApp-Sorteo/Data/ApplicationDbContext.cs
@@ -7,11 +7,11 @@ namespace WebApp_Sorteo.Data
 {
      public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
      {
-          public DbSet<Sorteo>? Sorteos { get; set; }
-          public DbSet<Ticket>? Tickets { get; set; }
-          public DbSet<Premio>? Premios { get; set; }
-          public DbSet<Pago>? Pagos { get; set; }
-          public DbSet<Usuario>? Usuarios { get; set; }
+          public required DbSet<Sorteo> Sorteos { get; set; }
+          public required DbSet<Ticket> Tickets { get; set; }
+          public required DbSet<Premio> Premios { get; set; }
+          public required DbSet<Pago> Pagos { get; set; }
+          public required DbSet<Usuario> Usuarios { get; set; }
           protected override void OnModelCreating(ModelBuilder builder)
           {
                base.OnModelCreating(builder);
