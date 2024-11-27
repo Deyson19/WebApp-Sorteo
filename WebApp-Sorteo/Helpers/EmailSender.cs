65eb7a067c7dc0f25873809f08a816e0378ba82c
Deyson Venté <deiandresvent@hotmail.com>
2024-11-27 17:33:57 -0500

    try catch y logger para envio de correos

--- WebApp-Sorteo/Helpers/EmailSender.cs
+++ WebApp-Sorteo/Helpers/EmailSender.cs
@@ -1,21 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using NuGet.Protocol;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 namespace WebApp_Sorteo.Helpers
 {
-    public class EmailSender(IConfiguration configuration) : IEmailSender
+    public class EmailSender(IConfiguration configuration, ILogger<EmailSender> logger) : IEmailSender
     {
         private readonly string SendGridSecret = configuration.GetValue<string>("Sendgrid:SecretKey")!;
+        private readonly ILogger<EmailSender> _logger = logger;
         private readonly string email = "deii@mail.com";
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var client = new SendGridClient(SendGridSecret);
-            var from = new EmailAddress(this.email, "Sistema de Tickets");
-            var to = new EmailAddress(email, $"Usuario: {email.ToUpper()}");
-            var mensaje = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
-
-            return client.SendEmailAsync(mensaje);
+            try
+            {
+                var client = new SendGridClient(SendGridSecret);
+                var from = new EmailAddress(this.email, "Sistema de Tickets");
+                var to = new EmailAddress(email, $"Usuario: {email.ToUpper()}");
+                var mensaje = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+                _logger.LogInformation($"Correo enviado: {mensaje.ToJson()}");
+                return client.SendEmailAsync(mensaje);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message,ex.ToJson());
+                throw;
+            }
         }
     }
 }
