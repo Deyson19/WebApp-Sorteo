995bd406767d0d09e50186a91ab721014624f808
Deyson Venté <deiandresvent@hotmail.com>
2024-11-27 19:55:40 -0500

    haciendo uso del constructor principal/primario

--- WebApp-Sorteo/Controllers/HomeController.cs
+++ WebApp-Sorteo/Controllers/HomeController.cs
@@ -1,26 +1,22 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApp_Sorteo.Models;
 
 namespace WebApp_Sorteo.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IConfiguration _configuration;
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-        [ResponseCache(Duration = 100, Location = ResponseCacheLocation.Client, NoStore = false)]
-        public IActionResult Index() => View();
-        public IActionResult Privacy() => View();
+     public class HomeController(ILogger<HomeController> logger) : Controller
+     {
+          private readonly ILogger<HomeController> _logger = logger;
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+          [ResponseCache(Duration = 100, Location = ResponseCacheLocation.Client, NoStore = false)]
+          public IActionResult Index() => View();
+          public IActionResult Privacy() => View();
+
+          [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+          public IActionResult Error()
+          {
+               return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+          }
+     }
 }
